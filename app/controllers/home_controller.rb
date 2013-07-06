4fe6565b06abb2787782e5efd989e3cf23a588a9
anromeo <anthonykzullo@gmail.com>
2013-07-06 11:05:05 -0700

    working on the splash page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,17 @@ class HomeController < ApplicationController
   layout "join"
 
   def index
+    @photos = Photo.all
     @invite_request = InviteRequest.new
     @disable_signin_link = true
     @disable_signup_link = true
     if current_user
       redirect_to mems_path
     end
+    render layout: false
+  end
+
+  def signup
+    @disable_signup_link = true
   end
 end
