8320d1be67be5171f70f44ff313a2624e32efe1d
anromeo <anthonykzullo@gmail.com>
2013-07-09 09:43:37 -0700

    removed unnecessary files

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,6 +3,7 @@ class Photo < ActiveRecord::Base
 
   has_attached_file :pic, styles: { small: "150x150>", medium: "400x400>"},
                           #:convert_options => { :all => "-blue-shift 1.5" },
+                          bucket: "adventure-memories-not-production",
                           :storage => :s3,
                           :s3_credentials => "#{Rails.root}/config/s3_credentials.yml",                          :bucket => "adventure-memories"
                           #url: "/:attachment/:id/:style/:basename.:extension"
